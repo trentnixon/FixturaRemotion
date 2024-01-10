c23ce12519cf03d0513ed4bfa344ecb05704518a
trentnixon <trentnixon@gmail.com>
2024-01-10 16:24:56 +1100

    update to v4

--- remotion.config.js
+++ remotion.config.js
@@ -2,7 +2,10 @@
 // Each option also is available as a CLI flag: https://remotion.dev/docs/cli
 // ! The configuration file does only apply if you render via the CLI !
 
-import {Config} from 'remotion';
-
-Config.setImageFormat('jpeg');
+//import {Config} from 'remotion';
+import {Config} from "@remotion/cli/config";
+//Config.setImageFormat('jpeg');
+//Config.setOverwriteOutput(true);
+Config.overrideWebpackConfig()
 Config.setOverwriteOutput(true);
+Config.setVideoImageFormat('jpeg');
